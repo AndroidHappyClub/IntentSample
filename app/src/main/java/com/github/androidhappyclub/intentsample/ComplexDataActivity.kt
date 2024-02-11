41ca6189ac2a54e899cb9b389573eb4f8b25aa6c
Vast Gui <sakurajimamai2020@qq.com>
2024-02-11 09:16:02 +0800

    update(app):更新示例代码

--- app/src/main/java/com/github/androidhappyclub/intentsample/ComplexDataActivity.kt
+++ app/src/main/java/com/github/androidhappyclub/intentsample/ComplexDataActivity.kt
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 AndroidHappyClub
+ * Copyright (c) 2024 AndroidHappyClub
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,18 +28,23 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.database.getStringOrNull
 import com.ave.vastgui.tools.graphics.BmpUtils
 import com.ave.vastgui.tools.os.getParcelable
 import com.ave.vastgui.tools.utils.AppUtils
 import com.ave.vastgui.tools.viewbinding.viewBinding
 import com.github.androidhappyclub.intentsample.databinding.ActivityComplexDataBinding
+import com.github.androidhappyclub.intentsample.logger.mLogFactory
 
 class ComplexDataActivity : AppCompatActivity(R.layout.activity_complex_data) {
 
     private val mBinding by viewBinding(ActivityComplexDataBinding::bind)
+    private val mLogger = mLogFactory.getLog(ComplexDataActivity::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        uriTemporaryPermission()
 
         intent.getStringArrayListExtra(LIST_DATA)?.apply {
             val arrayAdapter = ArrayAdapter(
@@ -54,7 +59,29 @@ class ComplexDataActivity : AppCompatActivity(R.layout.activity_complex_data) {
         mBinding.ivBmp.setImageBitmap(bmp)
     }
 
-    companion object{
+    /**
+     * [Uri 临时权限示例](https://www.yuque.com/mashangxiayu/gne1e3/lyp5f7rkie8rc0i3#h2anv)。
+     *
+     * 获取从其他应用传递的 URI 并访问相关数据。
+     */
+    private fun uriTemporaryPermission() {
+        intent.data?.apply pointer@{
+            val cursor = contentResolver
+                .query(this, arrayOf("name", "sex"), null, null, null)
+            if (null == cursor) {
+                mLogger.d("没有获取到 $this 的游标")
+                return@pointer
+            }
+            while (cursor.moveToNext()) {
+                val name = cursor.getStringOrNull(cursor.getColumnIndex("name"))
+                val sex = cursor.getStringOrNull(cursor.getColumnIndex("sex"))
+                mLogger.d("获取的学生姓名$name,性别为$sex")
+            }
+            cursor.close()
+        }
+    }
+
+    companion object {
         const val LIST_DATA = "LIST_DATA"
         const val IMAGE = "IMAGE"
     }
